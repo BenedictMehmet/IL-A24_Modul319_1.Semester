f99faa5eb5343c938fa968d07b3e0c8040d493fd
Mehmet <Oezdemir.Mehmet@bene-edu.ch>
2024-11-26 09:59:51 +0100

    add .

--- IL A24_Modul319_1.Semester/Woche9/Wiederholungen/Wiederholung1.cs
+++ IL A24_Modul319_1.Semester/Woche9/Wiederholungen/Wiederholung1.cs
@@ -18,7 +18,7 @@ namespace IL_A24_Modul319_1.Semester.Woche9.Wiederholungen
             k = i++ + j;       // k = 4 + 3, dann i = 5 (erst wird k berechnet, dann i erhöht)
             j = --k + i;       // k wird zuerst um 1 verringert, also k = 6, dann j = 6 + 5 = 11
             i = k-- - j;       // i = 6 - 11 = -5, dann k wird um 1 verringert, k = 5
-            k += i * j;        // k = 5 + (-5) * 11 = 5 - 55 = -50
+            k += i * j;        // k = 5 + (-5) * 11 = 5 - 55 = -501
 
             // Ausgabe der Endwerte
             Console.WriteLine($"i = {i}");
