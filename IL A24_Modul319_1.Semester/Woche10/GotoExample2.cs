ea17a43f5e9d057319d35f04313af811765dca6d
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2024-11-29 12:11:07 +0100

    add week 10

---
+++ IL A24_Modul319_1.Semester/Woche10/GotoExample2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A24_Modul319_1.Semester.Woche10
+{
+    internal class GotoExample2
+    {
+        static void Main()
+        {
+            Console.WriteLine("Beispiel für die Verwendung von goto in einem Switch-Case:");
+
+            int option;
+
+            // Schleife, die fortfährt, bis eine gültige Option eingegeben wird
+            while (true)
+            {
+                try
+                {
+                    // Benutzer nach einer Option fragen
+                    Console.Write("Bitte eine Option auswählen (1, 2 oder 3): ");
+                    option = Convert.ToInt32(Console.ReadLine()); // Benutzereingabe lesen
+
+                    // Switch-Case mit goto-Anweisungen
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Option 1 gewählt.");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Option 2 gewählt. Springe zu Option 3.");
+                            // Springe direkt zu Fall 3
+                            goto case 3;
+
+                        case 3:
+                            Console.WriteLine("Option 3 gewählt.");
+                            break;
+
+                        default:
+                            // Ungültige Option
+                            Console.WriteLine("Ungültige Option. Bitte geben Sie 1, 2 oder 3 ein.");
+                            continue; // Springt zurück zum Anfang der Schleife und fordert eine neue Eingabe
+                    }
+
+                    // Wenn eine gültige Option gewählt wurde, die Schleife verlassen
+                    break;
+                }
+                catch (FormatException)
+                {
+                    // Fehlermeldung bei ungültiger Eingabe (z.B. Text statt Zahl)
+                    Console.WriteLine("Ungültige Eingabe. Bitte eine gültige Zahl eingeben.");
+                }
+            }
+
+            Console.WriteLine("Switch-Case mit goto beendet.\n");
+        }
+    }
+}
---
+++ IL A24_Modul319_1.Semester/Woche10/LoopControlExamples.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A24_Modul319_1.Semester.Woche10
+{
+    internal class LoopControlExamples
+    {
+        static void Main(string[] args)
+        {
+            // Aufruf der Methode ProcessNumbers
+            DemonstrateBreakAndContinue();
+        }
+        public static void DemonstrateBreakAndContinue()
+        {
+            Console.WriteLine("Beispiel für die Verwendung von break und continue:");
+
+            // Eine Schleife, die von 1 bis 10 zählt
+            for (int i = 1; i <= 10; i++)
+            {
+                // Überprüfung, ob die aktuelle Zahl gerade ist
+                if (i % 2 == 0)
+                {
+                    // Wenn die Zahl gerade ist, wird der Rest des Codes in dieser Iteration übersprungen
+                    // und die Schleife springt zur nächsten Iteration
+                    Console.WriteLine("Gerade Zahl: " + i + " wird übersprungen.");
+                    continue; // Überspringt den Rest des Codes und setzt die Schleife mit der nächsten Zahl fort
+                }
+
+                // Ausgabe der aktuellen ungeraden Zahl
+                Console.WriteLine("Ungerade Zahl: " + i);
+
+                // Überprüfung, ob die Zahl 7 erreicht wurde
+                if (i == 7)
+                {
+                    // Wenn die Zahl 7 erreicht wird, wird die Schleife abgebrochen
+                    Console.WriteLine("Zahl 7 erreicht. Schleife wird abgebrochen.");
+                    break; // Beendet die Schleife vollständig
+                }
+            }
+
+            // Dieser Code wird nach dem Abbruch der Schleife ausgeführt
+            Console.WriteLine("Die Schleife wurde beendet.\n");
+        }
+    }
+}
