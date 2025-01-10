9cb6eeb2399148eb305ab67ce47731790709da96
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-01-10 14:55:40 +0100

    add more tasks

--- IL A24_Modul319_1.Semester/KW02_25/Übungen/Übungen.cs
+++ IL A24_Modul319_1.Semester/KW02_25/Übungen/Übungen.cs
@@ -19,13 +19,14 @@ namespace IL_A24_Modul319_1.Semester.KW02_25.Übungen
 
    4. Berechne das Quadrat. (Tipp: Quadratberechnung ist Länge * Länge)
 
+    Weitere Übungen:
 
+    5. Prüfen, ob eine Zahl eine Primzahl ist (bool-Rückgabe). Erstelle die Methode dazu
 
+    6. Bestimme eine Zahl. Bis zu dieser Zahl sollte die Konsole zählen.
+        Zum Beispiel bestimme ich 5, dann soll in der Konsole 1 2 3 4 5 stehen.
 
-
-
-
-
+    7. Erstelle eine Methode, die überprüft, ob eine eingegebene Zahl eine Primzahl ist.
 
 
 
